2cf3d86df0dd299aa09cebacdb145d6d3fbca7b8
patilyogesh36 <patilyogesh36@gmail.com>
2022-01-06 13:35:20 +0530

    Initial commit for Kafka Test project

---
+++ src/main/java/com/ygptech/ConsumerDemo.java
@@ -0,0 +1,40 @@
+package com.ygptech;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+    public static void main(String[] args) {
+        ConsumerDemo consumerDemo = new ConsumerDemo();
+        consumerDemo.consumer();
+    }
+
+    private void consumer() {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-second-group");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList("first_topic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord record : records) {
+                logger.info("Key : "+record.key()+",  value : "+record.value()+",  offset : "+record.offset()+",  partition : "+record.partition());
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/ygptech/ProducerDemo.java
@@ -0,0 +1,50 @@
+package com.ygptech;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Timestamp;
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        ProducerDemo producerDemo = new ProducerDemo();
+        producerDemo.produce();
+    }
+
+    public void produce() {
+        //properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //send data
+        for (int i=1; i<=10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "id_"+i,i+" message from intellij IDEA");
+            producer.send(record, (metadata, e) -> {
+                if (e == null) {
+                    logger.info("Received new metadata." +
+                            "\nTopic:" + metadata.topic() +
+                            "\nPartition:" + metadata.partition() +
+                            "\nOffset:" + metadata.offset() +
+                            "\nTimestamp:" + new Timestamp(System.currentTimeMillis()));
+                } else {
+                    logger.error("Error while producing.", e);
+                }
+            });
+        }
+        producer.flush();
+        producer.close();
+    }
+}
